ac12fe666f1cb0d64d6e45dadb097951de3ef603
Justin Boohan <jboohan@yahoo.co.uk>
2021-08-17 09:45:31 +0100

    add gradle project

---
+++ src/main/java/com/justinboohan/cuteurl/CuteurlApplication.java
@@ -0,0 +1,13 @@
+package com.justinboohan.cuteurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuteurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuteurlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justinboohan/cuteurl/CuteurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinboohan.cuteurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuteurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
